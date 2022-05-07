f6d61fd1e987a3b24155617706e6852ec1a2d0e8
clauridsen <55226881+clauridsen@users.noreply.github.com>
2022-05-07 10:01:49 +0200

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Dependencies installation:
+
+sudo apt-get install -y build-essential gdb g++ zip cmake
+sudo apt-get install -y libsdl2-dev
+sudo apt install -y libsdl2-image-dev
+sudo apt install -y libsdl2-ttf-dev
+sudo apt-get install -y libglew-dev
+sudo apt-get install libbullet-dev -y
+
+# RakNet installation:
+git clone https://github.com/facebookarchive/RakNet.git /home/pi/RakNet
+cd /home/pi/RakNet
