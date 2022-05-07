446a9cccb5ed3cad1a306bbdceec1314e57c35c4
clauridsen <55226881+clauridsen@users.noreply.github.com>
2022-05-07 22:00:14 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,11 @@
 
 # Dependencies installation:
 
+sudo apt-get update
+
+sudo apt-get upgrade --yes
+
+
 
 sudo apt-get install -y build-essential gdb g++ zip cmake
 sudo apt-get install -y libsdl2-dev
@@ -9,7 +14,7 @@ sudo apt install -y libsdl2-image-dev
 sudo apt install -y libsdl2-ttf-dev
 sudo apt-get install -y libglew-dev
 sudo apt-get install libbullet-dev -y
-sudo apt install git
+sudo apt install git --yes
 
 # RakNet installation:
 git clone https://github.com/clauridsen/RakNet.git /home/pi/RakNet
