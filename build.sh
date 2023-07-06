896cf29d72663bd72a41d55f476f092610e7d434
clauridsen <55226881+clauridsen@users.noreply.github.com>
2023-07-06 11:19:04 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -18,9 +18,16 @@ sudo apt install git --yes
 
 # RakNet installation:
 git clone https://github.com/clauridsen/RakNet.git /home/pi/RakNet
+cmake .
+make
+make install
 
+echo Removing old RakNet installation if it exists.
+sudo rm -rf /usr/local/include/raknet
 
-git clone https://github.com/johndcollins/RTTClient.git /home/pi/RTTClient
+echo Moving RakNet to the correct directory.
+sudo mv Lib/LibStatic/libRakNetLibStatic.a /usr/local/lib
+sudo mv include/raknet /usr/local/include/
 
+echo RakNet installed.
 
-sudo reboot
