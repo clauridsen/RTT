cceb3d81cfbd7c7ea94a2097d765dd928e816e58
clauridsen <55226881+clauridsen@users.noreply.github.com>
2023-07-07 08:54:11 +0200

    Update RakNetinstall.sh

--- RakNetinstall.sh
+++ RakNetinstall.sh
@@ -6,11 +6,5 @@ sudo cmake .
 sudo make
 sudo make install
 
-echo Removing old RakNet installation if it exists.
-sudo rm -rf /usr/local/include/raknet
-
-echo Moving RakNet to the correct directory.
-sudo mv Lib/LibStatic/libRakNetLibStatic.a /usr/local/lib
-sudo mv include/raknet /usr/local/include/
 
 echo RakNet installed.
