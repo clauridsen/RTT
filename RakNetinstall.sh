afd41131fe5143f639895a264fd53580a8010317
clauridsen <55226881+clauridsen@users.noreply.github.com>
2023-07-07 08:54:49 +0200

    Update RakNetinstall.sh

--- RakNetinstall.sh
+++ RakNetinstall.sh
@@ -1,3 +1,21 @@
+#!/bin/bash
+
+# Dependencies installation:
+
+sudo apt-get update
+
+sudo apt-get upgrade --yes
+
+
+
+sudo apt-get install -y build-essential gdb g++ zip cmake
+sudo apt-get install -y libsdl2-dev
+sudo apt install -y libsdl2-image-dev
+sudo apt install -y libsdl2-ttf-dev
+sudo apt-get install -y libglew-dev
+sudo apt-get install libbullet-dev -y
+sudo apt install git --yes
+
 # RakNet installation:
 sudo git clone https://github.com/clauridsen/RakNet.git
 cd RakNet
