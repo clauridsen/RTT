01b279a967df16ccc01e7a11c445aca805195dc9
clauridsen <55226881+clauridsen@users.noreply.github.com>
2024-12-20 08:17:57 +0100

    Update Install_RakNet.sh

--- Install_RakNet.sh
+++ Install_RakNet.sh
@@ -5,18 +5,37 @@ sudo apt-get update
 sudo apt-get upgrade --yes
 
 # Installer build-værktøjer og nødvendige biblioteker
-sudo apt-get install -y build-essential gdb g++ zip cmake
+sudo apt-get install -y build-essential gdb g++ zip
+
+# Check if git is installed, if not, install it
+if ! command -v git &> /dev/null
+then
+    echo "git could not be found, installing git."
+    sudo apt-get install -y git
+fi
+
+# Check if cmake is installed, if not, install it
+if ! command -v cmake &> /dev/null
+then
+    echo "cmake could not be found, installing cmake."
+    sudo apt-get install -y cmake
+fi
+
 sudo apt-get install -y libsdl2-dev libsdl2-image-dev libsdl2-ttf-dev
-sudo apt-get install -y libglew-dev libbullet-dev git
+sudo apt-get install -y libglew-dev libbullet-dev
 
 # RakNet installation
 echo "Cloning RakNet repository..."
-git clone https://github.com/clauridsen/RakNet.git
+if [ ! -d "RakNet" ]; then
+    git clone https://github.com/clauridsen/RakNet.git
+else
+    echo "RakNet repository already exists."
+fi
 cd RakNet
 
 # Opret en build-mappe og byg RakNet med CMake
 echo "Building RakNet..."
-mkdir build && cd build
+mkdir -p build && cd build
 cmake ..
 make
 
